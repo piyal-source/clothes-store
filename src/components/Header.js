b3b5aae80a50e8d71dab574d6bcebf3323ca7480
Piyal <chakravertypiyal@gmail.com>
2021-12-15 23:25:31 +0530

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Header() {
     return (
         <header>
-            <h1>Clothes Store</h1>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Clothes Store</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Categories</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Account</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     );
 }
